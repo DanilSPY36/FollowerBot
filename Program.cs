e217089d342f82680a4b789e986d31197de8dd93
Danil Nazarko <danilspy36@gmail.com>
2024-04-11 16:09:28 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+
+class Program
+{
+    private static ITelegramBotClient botClient;
+    private static ReceiverOptions receiverOptions;
+
+    static async Task Main()
+    {
+        botClient = new TelegramBotClient("7138521353:AAHkLo2liQBSwqwmjcHI6co-fw-svtS-yNw");
+        receiverOptions = new ReceiverOptions
+        {
+            AllowedUpdates = new[]
+            {
+                UpdateType.Message,
+            },
+            ThrowPendingUpdates = true,
+        };
+        using var cts = new CancellationTokenSource();
+
+        botClient.StartReceiving(UpdateHandler, ErrorHandler, receiverOptions, cts.Token);
+        Console.ReadKey();
+    }
+
+    private static async Task UpdateHandler(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        // Обязательно ставим блок try-catch, чтобы наш бот не "падал" в случае каких-либо ошибок
+        try
+        {
+            // Сразу же ставим конструкцию switch, чтобы обрабатывать приходящие Update
+            switch (update.Type)
+            {
+                case UpdateType.Message:
+                    {
+                        Console.WriteLine("Пришло сообщение!");
+                        return;
+                    }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+    }
+
+    private static Task ErrorHandler(ITelegramBotClient botClient, Exception error, CancellationToken cancellationToken)
+    {
+        // Тут создадим переменную, в которую поместим код ошибки и её сообщение 
+        var ErrorMessage = error switch
+        {
+            ApiRequestException apiRequestException
+                => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+            _ => error.ToString()
+        };
+
+        Console.WriteLine(ErrorMessage);
+        return Task.CompletedTask;
+    }
+
+}
