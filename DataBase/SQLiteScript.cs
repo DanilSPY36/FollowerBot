0067580f5a26cc051bd914eb7e4cd7d0bdac8c36
Danil Nazarko <danilspy36@gmail.com>
2024-04-12 23:52:14 +0300

    first test

---
+++ DataBase/SQLiteScript.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace FollowerBot.DataBase
+{
+    internal  class SQLiteScript
+    {
+        private readonly string connectionString = "Data Source=SurfCoffee.db";
+        public List<string> Food = new List<string>();
+        public void createDB()
+        {
+            using (var con = new SqliteConnection(connectionString))
+            {
+                con.Open();
+                SqliteCommand cmd = con.CreateCommand();
+                cmd.Connection = con;
+                cmd.CommandText = "SELECT name FROM sqlite_master WHERE type='table' AND name='Shippers'";
+                var tableName = cmd.ExecuteScalar();
+                if (tableName == null)
+                {
+                    cmd.CommandText = "CREATE TABLE Shippers(_ShipperId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, Name TEXT NOT NULL)";
+                    cmd.ExecuteNonQuery();
+                    cmd.CommandText = "INSERT INTO Shippers(Name) VALUES ('ИП Судин')";
+                    cmd.ExecuteNonQuery();
+                }
+
+                cmd.CommandText = "SELECT name FROM sqlite_master WHERE type='table' AND name='Food'";
+                tableName = cmd.ExecuteScalar();
+                if (tableName == null)
+                {
+                    cmd.CommandText = "CREATE TABLE Food (" +
+                                  "FoodId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, " +
+                                  "ShipperId INTEGER NOT NULL, " +
+                                  "Name TEXT NOT NULL, " +
+                                  "Description TEXT, " +
+                                  "Composition TEXT, " +
+                                  "Weight REAL, " +
+                                  "Proteins REAL, " +
+                                  "Fats REAL, " +
+                                  "Carbohydrates REAL, " +
+                                  "Calorie REAL)";
+                    cmd.ExecuteNonQuery();
+
+
+                    cmd.CommandText = "INSERT INTO Food (ShipperId, Name, Description, Composition, Weight, Proteins, Fats, Carbohydrates, Calorie) " +
+                              "VALUES (@ShipperId, @Name, @Description, @Composition, @Weight, @Proteins, @Fats, @Carbohydrates, @Calorie)";
+                    cmd.Parameters.AddWithValue("@ShipperId", 1);
+                    cmd.Parameters.AddWithValue("@Name", "КЕШЬЮ КЕЙК СЛИВОЧНЫЙ ПЛОМБИР С ТЫКВЕННЫМИ СЕМЕЧКАМИ");
+                    cmd.Parameters.AddWithValue("@Description", "Сочетание нежнейшего ванильного мусса и запеченных тыквенных семечек с морской солью будет понятной и приятной классикой для большинства ваших гостей, особенно когда в десерте нет сахара и глютена, 100% vegan");
+                    cmd.Parameters.AddWithValue("@Composition", "Кешью активированный, органические кокосовые сливки, масло кокосовое первого холодного отжима, сироп топинамбура, тыквенные семечки запеченные, миндаль обжаренный, финики, ванилин, соль");
+                    cmd.Parameters.AddWithValue("@Weight", 100.00);
+                    cmd.Parameters.AddWithValue("@Proteins", 10.11);
+                    cmd.Parameters.AddWithValue("@Fats", 36.61);
+                    cmd.Parameters.AddWithValue("@Carbohydrates", 19.06);
+                    cmd.Parameters.AddWithValue("@Calorie", 447.39);
+                    cmd.ExecuteNonQuery();
+                    cmd.Parameters.Clear();
+                    cmd.CommandText = "INSERT INTO Food (ShipperId, Name, Description, Composition, Weight, Proteins, Fats, Carbohydrates, Calorie) " +
+                              "VALUES (@ShipperId, @Name, @Description, @Composition, @Weight, @Proteins, @Fats, @Carbohydrates, @Calorie)";
+                    cmd.Parameters.AddWithValue("@ShipperId", 2);
+                    cmd.Parameters.AddWithValue("@Name", "КЕШЬЮ КЕЙК МАНГО");
+                    cmd.Parameters.AddWithValue("@Description", "Легкий, свежий десерт с натуральными кусочками манго  в сочетании со сливочностью кокоса и коржа из миндаля и фиников, без сахара и глютена полностью на растительной основе с любовью о вас и планете, 100% vegan");
+                    cmd.Parameters.AddWithValue("@Composition", "Манго быстрозамороженный, манго сушёный, миндаль обжаренный, финики, кешью активированный, органические кокосовые сливки, масло кокосовое первого холодного отжима, сироп топинамбура, натуральный ароматизатор манго");
+                    cmd.Parameters.AddWithValue("@Weight", 108);
+                    cmd.Parameters.AddWithValue("@Proteins", 7.28);
+                    cmd.Parameters.AddWithValue("@Fats", 26.47);
+                    cmd.Parameters.AddWithValue("@Carbohydrates", 35.59);
+                    cmd.Parameters.AddWithValue("@Calorie", 404.55);
+                    cmd.ExecuteNonQuery();
+                    cmd.Parameters.Clear();
+
+
+
+                    /*cmd.CommandText = "INSERT INTO Food (ShipperId, Name, Description, Composition, Weight, Proteins, Fats, Carbohydrates, Calorie) " +
+                              "VALUES (@ShipperId, @Name, @Description, @Composition, @Weight, @Proteins, @Fats, @Carbohydrates, @Calorie)";
+                    cmd.Parameters.AddWithValue("@ShipperId", 3);
+                    cmd.Parameters.AddWithValue("@Name", "ШОКОЛАДНЫЙ КЕШЬЮ КЕЙК (with sugar)");
+                    cmd.Parameters.AddWithValue("@Description", "-");
+                    cmd.Parameters.AddWithValue("@Composition", "Кешью, масло какао холодного отжима органическое, сливки кокосовые органические, какао порошок, ванилин, сироп топинамбура, миндаль, финик, лесной орех, сахар тростниковый.");
+                    cmd.Parameters.AddWithValue("@Weight", );
+                    cmd.Parameters.AddWithValue("@Proteins", );
+                    cmd.Parameters.AddWithValue("@Fats", );
+                    cmd.Parameters.AddWithValue("@Carbohydrates", );
+                    cmd.Parameters.AddWithValue("@Calorie", );
+                    cmd.ExecuteNonQuery(); */
+
+                    cmd.CommandText = "INSERT INTO Food (ShipperId, Name, Description, Composition, Weight, Proteins, Fats, Carbohydrates, Calorie) " +
+                              "VALUES (@ShipperId, @Name, @Description, @Composition, @Weight, @Proteins, @Fats, @Carbohydrates, @Calorie)";
+                    cmd.Parameters.AddWithValue("@ShipperId", 7);
+                    cmd.Parameters.AddWithValue("@Name", "АРАХИСОВОЕ ПИРОЖНОЕ В ШОКОЛАДЕ");
+                    cmd.Parameters.AddWithValue("@Description", "Десерт внутри которого карамель из органических кокосовых сливок с запечённым арахисом и сливочной начинкой облитая супер молочным, легким веган шоколадом оставит ещё надолго гостя под впечатлением и желанием вернуться за этим потрясающим вкусом!");
+                    cmd.Parameters.AddWithValue("@Composition", "Кешью активированный, органические кокосовые сливки, масло кокосовое первого холодного отжима, сироп топинамбура, сахар, глюкоза, кешью-шоколад (масло какао, нерафинированный тростниковый сахар-сырец, какао тертое, кешью жаренный, мука из зелёной гречки, кокосовая мука, кокосовый сахар, менее 0,1 эмульгатор лецитин подсолнечный, натуральный ароматизатор ваниль, соль розовая гималайская)");
+                    cmd.Parameters.AddWithValue("@Weight", 84);
+                    cmd.Parameters.AddWithValue("@Proteins", 6.52);
+                    cmd.Parameters.AddWithValue("@Fats", 28.03);
+                    cmd.Parameters.AddWithValue("@Carbohydrates", 25.93);
+                    cmd.Parameters.AddWithValue("@Calorie", 368.35);
+                    cmd.ExecuteNonQuery();
+                    cmd.Parameters.Clear();
+
+                    Console.WriteLine("Data has been inserted into the Food table");
+                }
+
+            }
+            Console.Read();
+        }
+        public List<string> getFood(int idShipper)
+        {
+            using (var con = new SqliteConnection(connectionString))
+            {
+                con.Open();
+                SqliteCommand cmd = con.CreateCommand();
+                cmd.Connection = con;
+                cmd.CommandText = "SELECT * FROM Food";
+                using (SqliteDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.HasRows) // если есть данные
+                    {
+                        while (reader.Read())   // построчно считываем данные
+                        {
+                            var id = reader.GetValue(0);
+                            var shipperId = reader.GetValue(1);
+                            var Name = reader.GetValue(2);
+                            var Description = reader.GetValue(3);
+                            var Composition = reader.GetValue(4);
+                            var Weight = reader.GetValue(5);
+                            var Proteins = reader.GetValue(6);
+                            var Fats = reader.GetValue(7);
+                            var Carbohydrates = reader.GetValue(8);
+                            var Calories = reader.GetValue(9);
+
+                            string info = $"ID: {id} || ShipperId: {shipperId} ||  Name: {Name} || " +
+                                $"Description: {Description} || Composition: {Composition} || " +
+                                $"Weight: {Weight} || Proteins: {Proteins} || Fats: {Fats} || " +
+                                $"Carbohydrates: {Carbohydrates} || Calories: {Calories} ";
+                            Food.Add(info);
+                        }
+                    }
+                }
+            }
+            return Food;
+        }
+    }
+}
+//"P:\Visual Studio 2022\sours\repos\Job_projects\FollowerBot\bin\Debug\net7.0\SurfCoffee.db"
--- Program.cs
+++ Program.cs
@@ -3,21 +3,25 @@ using Telegram.Bot.Exceptions;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
-
+using FollowerBot.DataBase;
+using Telegram.Bot.Types.ReplyMarkups;
 
 class Program
 {
     private static ITelegramBotClient botClient;
     private static ReceiverOptions receiverOptions;
-
+    private static SQLiteScript sql;
     static async Task Main()
     {
-        botClient = new TelegramBotClient("7138521353:AAHkLo2liQBSwqwmjcHI6co-fw-svtS-yNw");
+        sql = new SQLiteScript();
+        sql.createDB();
+        botClient = new TelegramBotClient("7190916687:AAG4L9eYwyj8bLJtXajo6uTP-k-MuIkRdIs");
         receiverOptions = new ReceiverOptions
         {
             AllowedUpdates = new[]
             {
                 UpdateType.Message,
+                UpdateType.CallbackQuery
             },
             ThrowPendingUpdates = true,
         };
@@ -29,15 +33,156 @@ class Program
 
     private static async Task UpdateHandler(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
     {
-        // Обязательно ставим блок try-catch, чтобы наш бот не "падал" в случае каких-либо ошибок
         try
         {
-            // Сразу же ставим конструкцию switch, чтобы обрабатывать приходящие Update
             switch (update.Type)
             {
                 case UpdateType.Message:
                     {
-                        Console.WriteLine("Пришло сообщение!");
+                        var message = update.Message;
+                        var user = message.From;
+                        var chat = message.Chat;
+
+                        switch (message.Type)
+                        {   
+                            case MessageType.Unknown:
+                                break;
+                            case MessageType.Text:
+                                if (message.Text == "/start")
+                                {
+                                    await botClient.SendTextMessageAsync(
+                                        chat.Id,
+                                        "Выбери клавиатуру:\n" +
+                                        "/inline\n" +
+                                        "/reply\n");
+                                    return;
+                                }
+                                if (message.Text == "/inline")
+                                {
+                                    // Тут создаем нашу клавиатуру
+                                    var inlineKeyboard = new InlineKeyboardMarkup(
+                                        new List<InlineKeyboardButton[]>() // здесь создаем лист (массив), который содрежит в себе массив из класса кнопок
+                                        {
+                                        // Каждый новый массив - это дополнительные строки,
+                                        // а каждая дополнительная строка (кнопка) в массиве - это добавление ряда
+
+                                        new InlineKeyboardButton[] // тут создаем массив кнопок
+                                        {
+                                            InlineKeyboardButton.WithCallbackData("список поставщиков:", "ShippersBtn"),
+                                        },
+                                        new InlineKeyboardButton[]
+                                        {
+                                            InlineKeyboardButton.WithCallbackData("ТТК", "ttkBtn")
+                                        },
+                                        });
+                                    await botClient.SendTextMessageAsync(
+                                    chat.Id,
+                                    "Это inline клавиатура!",
+                                    replyMarkup: inlineKeyboard); // Все клавиатуры передаются в параметр replyMarkup
+
+                                    return;
+                                }
+                                if (message.Text == "/reply")
+                                {
+                                    // Тут все аналогично Inline клавиатуре, только меняются классы
+                                    // НО! Тут потребуется дополнительно указать один параметр, чтобы
+                                    // клавиатура выглядела нормально, а не как абы что
+
+                                    var replyKeyboard = new ReplyKeyboardMarkup(
+                                        new List<KeyboardButton[]>()
+                                        {
+                                        new KeyboardButton[]
+                                        {
+                                            new KeyboardButton("Привет!"),
+                                            new KeyboardButton("Пока!"),
+                                        },
+                                        new KeyboardButton[]
+                                        {
+                                            new KeyboardButton("Позвони мне!")
+                                        },
+                                        new KeyboardButton[]
+                                        {
+                                            new KeyboardButton("Напиши моему соседу!")
+                                        }
+                                        })
+                                    {
+                                        // автоматическое изменение размера клавиатуры, если не стоит true,
+                                        // тогда клавиатура растягивается чуть ли не до луны,
+                                        // проверить можете сами
+                                        ResizeKeyboard = true,
+                                    };
+
+                                    await botClient.SendTextMessageAsync(
+                                        chat.Id,
+                                        "Это reply клавиатура!",
+                                        replyMarkup: replyKeyboard); // опять передаем клавиатуру в параметр replyMarkup
+
+                                    return;
+                                }
+                                    break;
+                            default:
+                                break;
+                        }
+                        return;
+                    }
+                case UpdateType.CallbackQuery: 
+                    {
+                        var callbackQuery = update.CallbackQuery;
+                        var user = callbackQuery.From;
+                        Console.WriteLine($"{user.FirstName} ({user.Id}) нажал на кнопку: {callbackQuery.Data}");
+                        var chat = callbackQuery.Message.Chat;
+                        switch (callbackQuery.Data)
+                        {
+                            // Data - это придуманный нами id кнопки, мы его указывали в параметре
+                            // callbackData при создании кнопок. У меня это button1, button2 и button3
+
+                            case "ShippersBtn":
+                                {
+                                    // В этом типе клавиатуры обязательно нужно использовать следующий метод
+                                    await botClient.AnswerCallbackQueryAsync(callbackQuery.Id);
+                                    // Для того, чтобы отправить телеграмму запрос, что мы нажали на кнопку
+                                    var chatId = callbackQuery.Message.Chat.Id;
+                                    var newInlineKeyboard = new InlineKeyboardMarkup
+                                        (
+                                             new List<InlineKeyboardButton[]>()
+                                                {
+                                                    new InlineKeyboardButton[]
+                                                    {
+                                                        InlineKeyboardButton.WithCallbackData("ИП Судин", "Shipper1"),
+                                                    },
+                                                    new InlineKeyboardButton[]
+                                                    {
+                                                        InlineKeyboardButton.WithCallbackData("...", "newButton2"),
+                                                    }
+                                                }
+                                        );
+                                    //await botClient.SendTextMessageAsync(chat.Id, $"Вы нажали на {callbackQuery.Data}");
+                                    await botClient.SendTextMessageAsync(chatId, "Поставщики", replyMarkup: newInlineKeyboard);
+                                    return;
+                                }
+                            case "Shipper1":
+                                {
+                                    await botClient.AnswerCallbackQueryAsync(callbackQuery.Id);
+                                    var chatId = callbackQuery.Message.Chat.Id;
+                                    for(int i = 0; i < 3; i++)
+                                    {
+                                        await botClient.SendTextMessageAsync(chat.Id, $"{sql.getFood(1)[i]}");
+                                    }
+                                    return;
+                                }
+
+                            case "ttkBtn":
+                                {
+                                    // А здесь мы добавляем наш сообственный текст, который заменит слово "загрузка", когда мы нажмем на кнопку
+                                    await botClient.AnswerCallbackQueryAsync(callbackQuery.Id, "Тут может быть ваш текст!");
+
+                                    await botClient.SendTextMessageAsync(
+                                        chat.Id,
+                                        $"Вы нажали на {callbackQuery.Data}");
+                                    return;
+                                }
+                        }
+
                         return;
                     }
             }
@@ -61,5 +206,6 @@ class Program
         Console.WriteLine(ErrorMessage);
         return Task.CompletedTask;
     }
+
 
 }
